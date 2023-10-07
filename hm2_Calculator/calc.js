6dd4f2c70cc82b14ad6b6914c4df897f1806f279
Pedroxer <danila.melnikov.53@gmail.com>
2023-10-07 16:17:30 +0300

    Done with calculations

---
+++ hm2_Calculator/calc.js
@@ -0,0 +1,58 @@
+function compute(){
+    let doc = document.getElementById("answ");
+    let docValuePost = infixToPostfix(doc.value);
+    console.log(docValuePost);
+    
+    const stack =[];
+    const operators = {
+        '+':(a,b) => a+b,
+        '-':(a,b) => a-b,
+        '*':(a,b) => a*b,
+        '/':(a,b) => a/b
+    };
+    for(let tok of docValuePost.split(' ')){
+        if(!isNaN(tok)){
+            stack.push(+tok);
+        }else if(tok in operators){
+            let b = stack.pop();
+            let a = stack.pop();
+            let result = operators[tok](a,b);
+            stack.push(result); 
+        }
+    }
+    doc.value = stack[0];
+}
+function infixToPostfix(expression) {
+    const operators = {
+      '+': 1,
+      '-': 1,
+      '*': 2,
+      '/': 2,
+      
+    };
+  
+    let output = '';
+    let operatorStack = [];
+  
+    for (let i = 0; i < expression.length; i++) {
+      const token = expression[i];
+  
+      if (!isNaN(token) && isFinite(token)) {
+        output += token + ' ';
+      } else if (token in operators) {
+        while (
+          operatorStack.length > 0 &&
+          operators[token] <= operators[operatorStack[operatorStack.length - 1]]
+        ) {
+          output += operatorStack.pop() + ' ';
+        }
+        operatorStack.push(token);
+      } 
+    }
+  
+    while (operatorStack.length > 0) {
+      output += operatorStack.pop() + ' ';
+    }
+  
+    return output.trim();
+  }
